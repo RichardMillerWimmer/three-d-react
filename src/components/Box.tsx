f475117e009cc8bf142fe9cb1a7e5ab5b6e45f89
Richard Miller <richardmmill@gmail.com>
2022-09-10 19:23:21 -0400

    onlick physics - api with cannon

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,9 +1,9 @@
 import { useBox } from "@react-three/cannon"
 
 const Box = () => {
-    const [boxRef] = useBox(() => ({ mass: 1, position: [0, 3, 0] }))
+    const [boxRef, boxApi] = useBox(() => ({ mass: 1, position: [0, 3, 0] }))
     return (
-        <mesh ref={boxRef} position={[0, 3, 0]} >
+        <mesh onClick={() => { boxApi.velocity.set(0, 2, 0) }} ref={boxRef} position={[0, 3, 0]} >
             <boxBufferGeometry attach='geometry' />
             <meshLambertMaterial attach='material' color='blue' />
         </mesh>
